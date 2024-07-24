88d6b21494eb0934f41f0b7bca7bd9669dcd4caa
geovko <octobermes@gmail.com>
2024-07-24 02:17:58 -0700

    fixed routes

--- routes/html.js
+++
@@ -1,9 +0,0 @@
-const pages = require("express").Router();
-
-// Get /notes returns notes.html
-pages.get("/notes", (req, res) => {});
-
-// Get * returns index.html
-pages.get("/", (req, res) => {});
-
-module.exports = pages;
